63f8e39dde4fe734e7be36b90be3d1a02bbcd6b0
rafsun-dev <rafsun.dev@gmail.com>
2021-03-15 21:19:50 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World \nRafsun Jany \n01580644525");
+        System.out.println("1 \t2");
+        System.out.println("3 \t4");
+        System.out.println("\"java programming\"");
+    }
+}
